ab4186f745cb24c1a2c8c1b5bb7d319318353f22
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-05 06:55:56 -0500

    Added seeds to migration table Customer Groups.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+c1 = Customergroup.create!(party: 'Hendersons', partyquantity: 2, customerstatus: 'Preferred')
+c2 = Customergroup.create!(party: 'Parkers and McMillens', partyquantity: 4, customerstatus: 'General')
+c3 = Customergroup.create!(party: 'Jessica, Jack, and Susan', partyquantity: 3, customerstatus: 'General')
+c4 = Customergroup.create!(party: 'Denzel Ellison', partyquantity: 1, customerstatus: 'Preferred')
+c5 = Customergroup.create!(party: 'Dan Jacobs and Janice Thomson', partyquantity: 2, customerstatus: 'Diamond VIP')
+c6 = Customergroup.create!(party: 'Wensingtons', partyquantity: 2, customerstatus: 'Elite Diamond VIP')
+c7 = Customergroup.create!(party: 'The Arlington Family', partyquantity: 5, customerstatus: 'General')
