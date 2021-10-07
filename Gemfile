1a8559cd196275779bddfbb7b8b46eee75c10dda
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-07 13:00:15 -0500

    Added routes to Customer Groups Controller.

--- Gemfile
+++ Gemfile
@@ -14,6 +14,11 @@ gem 'bcrypt', '~> 3.1.7'
 
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 gem 'rack-cors'
+# Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
+# gem 'jbuilder', '~> 2.7'
+
+# Use Active Storage variant
+gem 'image_processing', '~> 1.2'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
@@ -26,3 +31,6 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+gem "active_model_serializers", "~> 0.10.12"
+# gem 'image_processing', '~> 1.2'
--- app/controllers/customergroups_controller.rb
+++ app/controllers/customergroups_controller.rb
@@ -1,8 +1,46 @@
 class CustomergroupsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+    
     def index
         customergroups = Customergroup.all
         render json:  customergroups
     end
+
+    def show
+        customergroup = Customergroup.find_by(id: params[:id])
+        render json: customergroups
+    
+    end
+
+    def create
+      customergroups = Customergroup.create!(customergroups_params)
+      render json: customergroups, status: :created
+    end
+
+    def update
+      customergroup = Customergroup.find_by(id: params[:id])
+      customergroup.update(customergroups_params)
+      render json: customergroup
+    end
+
+   def destroy
+    customergroups = Customergroup.find_by(id: params[:id])
+    customergroups.destroy
+    head :no_content
+  end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "User not found" }, status: :not_found
+      end
+    
+      def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
+
+      def customergroups_params
+        params.permit(:customergroup_id, :party, :partyquantity, :customerstatus)
+      end
 end
