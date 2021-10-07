51852cecda6fbbc465a5741b1fd439d3b7a59f08
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-07 11:44:29 -0500

    Updated routing in routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :winepurchases, only: [:index, :show, :create]
+  resources :winepurchases, only: [:index, :show, :create, :update]
   resources :artpieces, only: [:index, :show, :create, :update, :destroy]
-  resources :artsessions, only: [:index, :show, :create, :update, :destroy]
+  resources :artsessions, only: [:index, :show, :create, :update]
   resources :brandofwines, only: [:index, :show, :create, :update, :destroy]
   resources :customergroups, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
