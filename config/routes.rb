7a08565c97a02ea04247f85ade974e6525bba4cd
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-07 11:43:22 -0500

    Added routes to routes.rb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :winepurchases
-  resources :artpieces
-  resources :artsessions
-  resources :brandofwines
-  resources :customergroups
+  resources :winepurchases, only: [:index, :show, :create]
+  resources :artpieces, only: [:index, :show, :create, :update, :destroy]
+  resources :artsessions, only: [:index, :show, :create, :update, :destroy]
+  resources :brandofwines, only: [:index, :show, :create, :update, :destroy]
+  resources :customergroups, only: [:index, :show, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
