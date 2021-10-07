4fc265fff1e49a150f5dae44fe8e54fd8b533c37
Sparkles2000 <rucker.anna2000@gmail.com>
2021-10-07 13:16:28 -0500

    Added routes to Brand Of Wine Controller.

--- app/controllers/brandofwines_controller.rb
+++ app/controllers/brandofwines_controller.rb
@@ -5,4 +5,41 @@ class BrandofwinesController < ApplicationController
         brandofwines = Brandofwine.all
         render json:  brandofwines
     end
+
+    def show
+       brandofwine = Brandofwine.find_by(id: params[:id])
+        render json:brandofwine
+    
+    end
+
+    def create
+     brandofwine = Brandofwine.create!(brandofwine_params)
+      render json:brandofwine, status: :created
+    end
+
+    def update
+     brandofwine = Brandofwine.find_by(id: params[:id])
+     brandofwine.update(brandofwine_params)
+      render json:brandofwine
+    end
+
+   def destroy
+   brandofwine = Brandofwine.find_by(id: params[:id])
+   brandofwine.destroy
+    head :no_content
+  end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "User not found" }, status: :not_found
+      end
+    
+      def render_unprocessable_entity_response(invalid)
+        render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+      end
+
+      defbrandofwine_params
+        params.permit(:brandofwine_id, :brand, :age, :winetype, :price, :drysweet, :alcoholcontent, :img_src)
+      end
 end
--- app/controllers/customergroups_controller.rb
+++ app/controllers/customergroups_controller.rb
@@ -1,10 +1,10 @@
 class CustomergroupsController < ApplicationController
     rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
-    
+
     def index
         customergroups = Customergroup.all
-        render json:  customergroups
+        render json: customergroups
     end
 
     def show
